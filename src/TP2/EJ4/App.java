52305b356c8c5de9869c20b3729be716fad02af5
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-03-24 18:36:42 -0300

    Other form of ej4 tp2

--- src/TP2/EJ4/App.java
+++ src/TP2/EJ4/App.java
@@ -3,6 +3,11 @@ package TP2.EJ4;
 public class App {
     public static void main(String[] args) {
         secFibonacci(15,0,1);
+        System.out.println(" ");
+        System.out.println(" --- ");
+        for (int i = 0; i < 15; i++){
+            System.out.print(fibonacchi(i) + " " );
+        }
     }
 
     public static void secFibonacci(int ite,int ant1, int ant2){
@@ -11,5 +16,11 @@ public class App {
         }
         System.out.print(ant1 + " ");
         secFibonacci(ite - 1, ant2, ant1+ant2);
+    }
+    public static int fibonacchi(int n){
+        if (n < 2){
+            return n;
+        }
+        return fibonacchi(n-1) + fibonacchi(n-2);
     }
 }
