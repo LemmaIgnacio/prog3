d7360f13e6c04032f1f8238728413ebe83857e49
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-04-01 19:14:44 -0300

    add ej2 tp3

--- src/TP3/EJ2/Tree.java
+++ src/TP3/EJ2/Tree.java
@@ -10,6 +10,24 @@ public class Tree {
             this.root = null;
         }
 
+        public Integer getSuma (){
+            return getSuma(this.root);
+        }
+        private Integer getSuma(TreeNode node){
+            if (node == null){
+                return 0;
+            }
+            if (node.getLeft() == null && node.getRight() == null ){
+                return 0;
+            }
+            //valor actual + valor de mi derecha + valor de mi izquierda
+            return node.getValue() + getSuma(node.getLeft()) + getSuma(node.getRight());
+        }
+
+
+
+
+
         public Integer getRoot() {
             /*
             accede directamente a la raiz O(1);
